bce78a84ef26d2d5ad661db65eb97384b92a1628
nlabandera <nlabandera@countercraftsec.com>
2022-06-14 16:57:41 +0200

    user vote formulary

--- apps/howhot/src/app/app.module.ts
+++ apps/howhot/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, VoteComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
