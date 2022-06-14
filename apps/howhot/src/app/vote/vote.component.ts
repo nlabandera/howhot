4a67c083e1bc416cfdadbdecc3320308930f2142
nlabandera <nlabandera@countercraftsec.com>
2022-06-14 16:56:57 +0200

    user vote layout

---
+++ apps/howhot/src/app/vote/vote.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'howhot-vote',
+  templateUrl: './vote.component.html',
+  styleUrls: ['./vote.component.scss'],
+})
+export class VoteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
