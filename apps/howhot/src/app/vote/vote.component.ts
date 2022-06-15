924049176f636ed1cc48ec8a39d9520de0d625e0
nlabandera <nlabandera@countercraftsec.com>
2022-06-15 14:23:39 +0200

    form for voting and insert votes in the db

--- apps/howhot/src/app/vote/vote.component.ts
+++ apps/howhot/src/app/vote/vote.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Vote } from '@howhot/api-interfaces';
 import { VoteService } from './vote.service';
 
@@ -11,8 +12,9 @@ import { VoteService } from './vote.service';
 export class VoteComponent implements OnInit {
   votes: Vote[]= [];
   email!: string;
+  states: string[]=['ok','cold','hot'];
   vote!: string;
-  id!: number
+  id!: number;
 
   constructor(
     private voteService: VoteService,
@@ -31,12 +33,12 @@ export class VoteComponent implements OnInit {
     this.voteService.postVote({email, vote} as Vote).subscribe(
       data => {this.votes.push(data)}
     )
-  }
 
+  }
 
   ngOnInit(): void {
     console.log('works')
-    this.postVote()
+
     // this.http.post<Vote[]>('/api/votes',{email: "test@test.com", vote: "ok"}).subscribe(req => this.votes = req);
   }
 }
