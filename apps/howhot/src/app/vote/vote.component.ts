72be304746a18a251fbfeaab151061908f2b402b
nlabandera <nlabandera@countercraftsec.com>
2022-06-15 10:07:43 +0200

    post data from the formulary into de api

--- apps/howhot/src/app/vote/vote.component.ts
+++ apps/howhot/src/app/vote/vote.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Vote } from '@howhot/api-interfaces';
 import { VoteService } from './vote.service';
 
 @Component({
@@ -8,21 +9,33 @@ import { VoteService } from './vote.service';
   styleUrls: ['./vote.component.scss'],
 })
 export class VoteComponent implements OnInit {
-
+  votes: Vote[]= [];
   email!: string;
   vote!: string;
   id!: number
 
   constructor(
-    private voteService: VoteService
+    private voteService: VoteService,
+    private http: HttpClient
   ) {}
 
+  /* v1 */
   postVote(){
-    this.voteService.post([{email: this.email, vote: this.vote}])
-    console.log('new vote:'+this.email, this.vote)
+    // this.voteService.post({email: "test3@tmail.com", vote: "ok"})
+    this.voteService.post({email: this.email, vote:this.vote})
   }
+
+  /* v2 */
+  post(email:string, vote:string):void{
+    this.voteService.postVote({email, vote} as Vote).subscribe(
+      data => {this.votes.push(data)}
+    )
+  }
+
 
   ngOnInit(): void {
     console.log('works')
+    this.postVote()
+    // this.http.post<Vote[]>('/api/votes',{email: "test@test.com", vote: "ok"}).subscribe(req => this.votes = req);
   }
 }
