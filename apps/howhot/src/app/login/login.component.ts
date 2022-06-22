6e0ec2eb2bf1e18d9664aa14919fea6accfdc273
nlabandera <nlabandera@countercraftsec.com>
2022-06-22 16:26:02 +0200

    first login form functional

---
+++ apps/howhot/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { VoteService } from '../vote/vote.service';
+
+
+@Component({
+  selector: 'howhot-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+
+  email!: string;
+  vote!:string;
+
+  constructor(
+    private voteService: VoteService
+  ){}
+
+  login(){
+    this.voteService.post({email: this.email, vote: ''})
+  }
+
+}
